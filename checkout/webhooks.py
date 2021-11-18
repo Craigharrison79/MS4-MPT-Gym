45dc502103b36b7ad567ae39f9f43898092c93c7
Craigharrison79 <craigharrison7917@hotmail.com>
2021-11-18 14:42:08 +0000

    Finish webhooks for stripe and adding countries package.

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
