46f9efa276e14e324433a9eca2e3a0991351fb47
Craigharrison79 <craigharrison7917@hotmail.com>
2021-12-15 19:08:01 +0000

    Removing data from database that was showing up even though it had been deleted.

--- products/models.py
+++ products/models.py
@@ -44,6 +44,6 @@ class Product(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.name
