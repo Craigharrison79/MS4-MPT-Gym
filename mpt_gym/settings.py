21f38c2ced4f9d775ba29e0cbdb8de1bbc75fc87
Craigharrison79 <craigharrison7917@hotmail.com>
2021-12-16 21:31:45 +0000

    Removed secret key and set debug.

--- mpt_gym/settings.py
+++ mpt_gym/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)gd1jb&mv1gdt&qa6alpf15t#de2#l=xpf#a&vtdp+t3r*=qv7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-mpt-gym.herokuapp.com', 'localhost']
 
